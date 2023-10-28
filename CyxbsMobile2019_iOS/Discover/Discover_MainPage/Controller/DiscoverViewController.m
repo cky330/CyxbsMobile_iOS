1242d7426ae689081039c2540bc1314a0b2b165f
cky330 <670579217@qq.com>
2023-10-28 19:38:51 +0800

    [optimize]没课约

--- CyxbsMobile2019_iOS/Discover/Discover_MainPage/Controller/DiscoverViewController.m
+++ CyxbsMobile2019_iOS/Discover/Discover_MainPage/Controller/DiscoverViewController.m
@@ -10,7 +10,7 @@
 #import "DiscoverViewController.h"
 #import "LoginVC.h"
 #import "CheckInViewController.h"
-#import "WeDateViewController.h"           // 没课约
+//#import "WeDateViewController.h"           // 没课约
 #import "CQUPTMapViewController.h"         // 地图
 #import "FinderToolViewController.h"       // 工具
 #import "TODOMainViewController.h"         // 邮子清单
@@ -117,6 +117,8 @@ typedef NS_ENUM(NSUInteger, LoginStates) {
 
         [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(presentMySchedul) name:@"DiscoverVCShouldPresentMySchedul" object:nil];
     }
+    
+    [JudgeArrangeMessage judgetabBarRedDot];
 }
 
 - (void)viewDidLoad {
@@ -372,9 +374,10 @@ static int requestCheckinInfo = 0;
 
 - (void)touchNoClassAppointment {
     NSLog(@"点击了没课约");
-    UserItem *item = [[UserItem alloc] init];
-    WeDateViewController *vc = [[WeDateViewController alloc] initWithInfoDictArray:
-                                [@[@{@"name":item.realName, @"stuNum":item.stuNum}] mutableCopy]];
+//    UserItem *item = [[UserItem alloc] init];
+    WeDateVC *vc = [[WeDateVC alloc] init];
+//    WeDateViewController *vc = [[WeDateViewController alloc] initWithInfoDictArray:
+//                                [@[@{@"name":item.realName, @"stuNum":item.stuNum}] mutableCopy]];
     vc.hidesBottomBarWhenPushed = YES;
     [self.navigationController pushViewController:vc animated:YES];
 }
